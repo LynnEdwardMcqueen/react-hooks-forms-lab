a81f8267c67ae95a7d2da3c73a8b99ebbd7bacaa
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-05-31 20:57:21 -0600

    Adds search functionality for filtering

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,53 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
+let searchBuffer = ""
+let searchString = ""
+
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText ] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  
+  function handleSearchChange(event) {
+    event.preventDefault();
+    
+    searchBuffer = event.target.value
+ //  if (event.key != 'Enter') {
+  //    setSearchText(searchBuffer)
+  //  }  else {
+  //    setSearchText("Search...")
+  //    searchBuffer = null
+  
 
-    return item.category === selectedCategory;
+//    if (event.key === 'Enter') {
+    
+    setSearchText(event.target.value)
+//    } else {
+      return;
+ //   }
+  }
+
+
+
+  const itemsToDisplay = items.filter((item) => {
+    if (item.name.search(searchText) != -1) {
+      if (selectedCategory === "All")  return true;
+
+      return item.category === selectedCategory;
+    } else {
+      return false;
+    }
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange = {handleSearchChange} search = {searchText}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
@@ -30,3 +60,4 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+export {searchString}
